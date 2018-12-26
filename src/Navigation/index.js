3dc91b9fb01841f0843ad66d497abe858f0e161e
magc <magc@itu.dk>
2018-12-26 12:32:55 +0100

    Authentication & Session logic

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -2,8 +2,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SignOutButton from "../SignOut";
 import * as ROUTES from "../constants/routes";
+import AuthUserContext from "../Session/context";
 
-const Navigation = () => (
+const Navigation = ({ authUser }) => (
+    <div>
+        <AuthUserContext.Consumer>
+            {authUser =>
+                authUser ? <NavigationAuth /> : <NavigationNonAuth />
+            }
+        </AuthUserContext.Consumer>
+    </div>
+);
+
+const NavigationNonAuth = () => (
+    <div>
+        <ul>
+            <li>
+                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.LANDING}>Landing</Link>
+            </li>
+        </ul>
+    </div>
+);
+
+const NavigationAuth = () => (
     <div>
         <ul>
             <li>
--- src/index.js
+++ src/index.js
@@ -1,40 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navigation from "./Navigation";
 import Firebase, { FirebaseContext } from "./Firebase";
-
-import LandingPage from "./Landing";
-import SignUpPage from "./SignUp";
-import SignInPage from "./SignIn";
-import PasswordForgetPage from "./PasswordForget";
-import HomePage from "./Home";
-import AccountPage from "./Account";
-import AdminPage from "./Admin";
-
-import * as ROUTES from "./constants/routes";
-
-const App = () => (
-    <Router>
-        <div>
-            <Navigation />
-            <hr />
-
-            <Route exact path={ROUTES.LANDING} component={LandingPage} />
-            <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-            <Route
-                path={ROUTES.PASSWORD_FORGET}
-                component={PasswordForgetPage}
-            />
-            <Route path={ROUTES.HOME} component={HomePage} />
-            <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-            <Route path={ROUTES.ADMIN} component={AdminPage} />
-        </div>
-    </Router>
-);
+import App from "./App/index";
 
 ReactDOM.render(
     <FirebaseContext.Provider value={new Firebase()}>
